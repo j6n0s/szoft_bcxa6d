45834110d6daee9043cd150c9d52336d64e6442f
j6n0s <desko.zoltan86@gmail.com>
2024-03-28 18:59:49 +0100

    otthini tesztek memória lefoglalás

--- KígyósJátékOtthon/Form1.cs
+++ KígyósJátékOtthon/Form1.cs
@@ -60,7 +60,7 @@ namespace KígyósJátékOtthon
         //Alma elhelyezése
         void Alma_elhelyezése()
         {
-            Random rnd = new Random();
+            Random rnd = new Random(); 
             while (true)
             {
                 bool alma_ismétlődés = false;
---
+++ MemoriaLefoglalas/Form1.cs
@@ -0,0 +1,18 @@
+namespace MemoriaLefoglalas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            while (true) 
+            {
+                int a = new int();
+            }
+        }
+    }
+}
