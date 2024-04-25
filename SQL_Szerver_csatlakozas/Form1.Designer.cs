338d0dde73050c42653d132eb19ca20704150150
j6n0s <desko.zoltan86@gmail.com>
2024-04-25 12:58:37 +0200

    (9) órai feladat II pt

--- SQL_Szerver_csatlakozas/Form1.Designer.cs
+++ SQL_Szerver_csatlakozas/Form1.Designer.cs
@@ -28,19 +28,147 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
+            dataGridView1 = new DataGridView();
+            idDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            neptunDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            nameDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            birthDateDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            averageGradeDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            isActiveDataGridViewCheckBoxColumn = new DataGridViewCheckBoxColumn();
+            studentBindingSource = new BindingSource(components);
+            button1 = new Button();
+            button2 = new Button();
+            textBox1 = new TextBox();
+            listBox1 = new ListBox();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)studentBindingSource).BeginInit();
             SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dataGridView1.AutoGenerateColumns = false;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { idDataGridViewTextBoxColumn, neptunDataGridViewTextBoxColumn, nameDataGridViewTextBoxColumn, birthDateDataGridViewTextBoxColumn, averageGradeDataGridViewTextBoxColumn, isActiveDataGridViewCheckBoxColumn });
+            dataGridView1.DataSource = studentBindingSource;
+            dataGridView1.Location = new Point(12, 207);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(830, 291);
+            dataGridView1.TabIndex = 0;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            idDataGridViewTextBoxColumn.HeaderText = "Id";
+            idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            // 
+            // neptunDataGridViewTextBoxColumn
+            // 
+            neptunDataGridViewTextBoxColumn.DataPropertyName = "Neptun";
+            neptunDataGridViewTextBoxColumn.HeaderText = "Neptun";
+            neptunDataGridViewTextBoxColumn.Name = "neptunDataGridViewTextBoxColumn";
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            // 
+            // birthDateDataGridViewTextBoxColumn
+            // 
+            birthDateDataGridViewTextBoxColumn.DataPropertyName = "BirthDate";
+            birthDateDataGridViewTextBoxColumn.HeaderText = "BirthDate";
+            birthDateDataGridViewTextBoxColumn.Name = "birthDateDataGridViewTextBoxColumn";
+            // 
+            // averageGradeDataGridViewTextBoxColumn
+            // 
+            averageGradeDataGridViewTextBoxColumn.DataPropertyName = "AverageGrade";
+            averageGradeDataGridViewTextBoxColumn.HeaderText = "AverageGrade";
+            averageGradeDataGridViewTextBoxColumn.Name = "averageGradeDataGridViewTextBoxColumn";
+            // 
+            // isActiveDataGridViewCheckBoxColumn
+            // 
+            isActiveDataGridViewCheckBoxColumn.DataPropertyName = "IsActive";
+            isActiveDataGridViewCheckBoxColumn.HeaderText = "IsActive";
+            isActiveDataGridViewCheckBoxColumn.Name = "isActiveDataGridViewCheckBoxColumn";
+            // 
+            // studentBindingSource
+            // 
+            studentBindingSource.DataSource = typeof(Models.Student);
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            button1.Location = new Point(722, 164);
+            button1.Name = "button1";
+            button1.Size = new Size(119, 32);
+            button1.TabIndex = 1;
+            button1.Text = "mentés";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(12, 131);
+            button2.Name = "button2";
+            button2.Size = new Size(229, 29);
+            button2.TabIndex = 2;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(26, 67);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(215, 23);
+            textBox1.TabIndex = 3;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(337, 71);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(263, 109);
+            listBox1.TabIndex = 4;
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(852, 509);
+            Controls.Add(listBox1);
+            Controls.Add(textBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(dataGridView1);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)studentBindingSource).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn neptunDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn birthDateDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn averageGradeDataGridViewTextBoxColumn;
+        private DataGridViewCheckBoxColumn isActiveDataGridViewCheckBoxColumn;
+        private BindingSource studentBindingSource;
+        private Button button1;
+        private Button button2;
+        private TextBox textBox1;
+        private ListBox listBox1;
     }
 }
--- SQL_Szerver_csatlakozas/Form1.cs
+++ SQL_Szerver_csatlakozas/Form1.cs
@@ -1,13 +1,64 @@
+using SQL_Szerver_csatlakozas.Models;
+
 namespace SQL_Szerver_csatlakozas
 {
     public partial class Form1 : Form
     {
+        Models.StudentContext context = new Models.StudentContext();
+
         public Form1()
         {
             InitializeComponent();
+            studentBindingSource.DataSource = context.Students.ToList();
+            dataGridView1.DataSource = studentBindingSource.DataSource;
         }
 
         private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            listBox1.DataSource = (from x in context.Students select x.Neptun).ToList();
+            listBox1.DoubleClick += ListBox1_DoubleClick;
+        }
+
+        private void ListBox1_DoubleClick(object? sender, EventArgs e)
+        {
+            var jelenlegi = listBox1.SelectedItem as string;
+            var eredmény = from x in context.Students
+                           where x.Neptun == $"{jelenlegi}"
+                           select x;
+
+            studentBindingSource.DataSource = eredmény.ToList();
+            dataGridView1.DataSource = studentBindingSource.DataSource;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.InnerException.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            var eredmény = from x in context.Students
+                           where x.Name.StartsWith(textBox1.Text)
+                           select x;
+
+            studentBindingSource.DataSource = eredmény.ToList();
+            dataGridView1.DataSource = studentBindingSource.DataSource;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
